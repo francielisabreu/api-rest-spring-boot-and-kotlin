1e1fd71e992b2681d42e36848781b6ca4a33abcc
francielisabreu <contato.theframboesa@gmail.com>
2022-09-27 19:59:12 -0300

    firt step spring boot with Kotlin

---
+++ firtstep-spring/api-rest-spring-boot-and-kotlin/src/main/kotlin/br/com/myrest/Greeting.kt
@@ -0,0 +1,6 @@
+package br.com.myrest
+
+class Greeting (
+    val id: Long,
+    val content: String
+    )
---
+++ firtstep-spring/api-rest-spring-boot-and-kotlin/src/main/kotlin/br/com/myrest/GreetingController.kt
@@ -0,0 +1,19 @@
+package br.com.myrest
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.atomic.AtomicLong
+
+@RestController
+class GreetingController {
+
+    val counter: AtomicLong = AtomicLong()
+
+    @RequestMapping("/greeting")
+    fun greeting(@RequestParam(value="name", defaultValue = "world")name: String?): Greeting {
+        return Greeting(counter.incrementAndGet(),"Hello $name")
+    }
+
+
+}
